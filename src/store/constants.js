ddf29fc8a1860de51bcba662d89261bbe0ea7729
timattt <timattt@gmail.com>
2024-06-02 12:34:22 +0300

    resolving timattt/TypeSixCloud#8
    1. changed url to public
    2. changed to mipt.io

--- src/store/constants.js
+++ src/store/constants.js
@@ -4,7 +4,7 @@ export const CLIENT_SECRET = process.env.REACT_APP_TYPE12_CLIENT_SECRET
 export const REDIRECT_URI = process.env.REACT_APP_TYPE12_REDIRECT_URI
 export const TYPE11_URL = process.env.REACT_APP_TYPE12_TYPE11_URL
 
-export const OAUTH2_TOKEN_ENDPOINT = "/sso/oauth2/token"
-export const OAUTH2_AUTHORIZATION_ENDPOINT = "/sso/oauth2/authorize"
-export const LOGOUT_ENDPOINT = "/sso/logout"
-export const INTROSPECTION_ENDPOINT = '/sso/oauth2/introspect'
+export const OAUTH2_TOKEN_ENDPOINT = "/public/oauth2/token"
+export const OAUTH2_AUTHORIZATION_ENDPOINT = "/public/oauth2/authorize"
+export const LOGOUT_ENDPOINT = "/public/logout"
+export const INTROSPECTION_ENDPOINT = '/public/oauth2/introspect'
